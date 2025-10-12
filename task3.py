36e909626fcc4a43211d694efd8b010b667664db
LokEnce333 <ilya.evstratoff@yandex.ru>
2025-10-12 21:39:24 +0300

    Task №3

--- task3.py
+++ task3.py
@@ -1,28 +1,28 @@
-# # Задание 1
-# x = int(input())
+# Задание 1
+x = int(input())
 
-# while(x % 2 and x % 10 != 5):
-#     x = int(input())
-
-
-# # Задание 2
-# for i in range(10):
-#     print(i)
+while(x % 2 and x % 10 != 5):
+    x = int(input())
 
 
-# # Задание 3
-# K = int(input())
-# N = int(input())
-
-# sumy = 0
-# while(K <= N):
-#     if(K % 2):
-#         sumy += K
-#     K += 1
-# print(sumy)
+# Задание 2
+for i in range(10):
+    print(i)
 
 
-# Задание 4
+# Задание 3
+K = int(input())
+N = int(input())
+
+sumy = 0
+while(K <= N):
+    if(K % 2):
+        sumy += K
+    K += 1
+print(sumy)
+
+
+# Задание 3
 N = int(input())
 
 factorial = 1
