bf874f4d5d964da9d234f5ad370b1c71cdf5078e
LokEnce333 <ilya.evstratoff@yandex.ru>
2025-10-12 21:38:36 +0300

    Task №2

---
+++ task3.py
@@ -0,0 +1,31 @@
+# # Задание 1
+# x = int(input())
+
+# while(x % 2 and x % 10 != 5):
+#     x = int(input())
+
+
+# # Задание 2
+# for i in range(10):
+#     print(i)
+
+
+# # Задание 3
+# K = int(input())
+# N = int(input())
+
+# sumy = 0
+# while(K <= N):
+#     if(K % 2):
+#         sumy += K
+#     K += 1
+# print(sumy)
+
+
+# Задание 4
+N = int(input())
+
+factorial = 1
+for i in range(1, N+1):
+    factorial *= i
+print(factorial)
