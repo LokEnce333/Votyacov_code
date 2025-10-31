61e05f695e17bc9e7bda185d3e9362df7e80f358
LokEnce333 <ilya.evstratoff@yandex.ru>
2025-10-31 23:09:08 +0300

    Task #6

--- task6.py
+++ task6.py
@@ -1,50 +1,50 @@
-# # Задача 1
-# n = int(input())
-# m = int(input())
+# Задача 1
+n = int(input())
+m = int(input())
 
-# arr = []
-# arr.append(n)
-# arr.append(m)
-# print(arr, arr[0] + arr[1])
-# #Ответ: [12, 34] 46
-
-
-# # Задача 2
-# s = input()
-# arr = []
-
-# for i in s.split():
-#     arr.append(i)
-# print(arr[0], arr[-1])
-# #Ответ: Программирование школы
+arr = []
+arr.append(n)
+arr.append(m)
+print(arr, arr[0] + arr[1])
+#Ответ: [12, 34] 46
 
 
-# # Задача 3
-# s = input()
-# arr = []
+# Задача 2
+s = input()
+arr = []
 
-# for i in s.split():
-#     arr.append(i)
+for i in s.split():
+    arr.append(i)
+print(arr[0], arr[-1])
+#Ответ: Программирование школы
+
+
+# Задача 3
+s = input()
+arr = []
+
+for i in s.split():
+    arr.append(i)
     
-# maxy = -1
-# word = ''
-# for i in arr:
-#     if maxy < len(i):
-#         maxy = len(i)
-#         word = i
-# print(word)
-# #Ответ: нетипичный
+maxy = -1
+word = ''
+for i in arr:
+    if maxy < len(i):
+        maxy = len(i)
+        word = i
+print(word)
+#Ответ: нетипичный
 
 
-# # Задача 4
-# n = int(input())
+# Задача 4
+n = int(input())
 
-# arr = []
-# for i in range(1, n+1):
-#     if (i % 3 == 0) or (i % 5 == 0):
-#         arr.append(i)
-# print(sum(arr))
-# #Ответ: 234168
+arr = []
+for i in range(1, n+1):
+    if (i % 3 == 0) or (i % 5 == 0):
+        arr.append(i)
+print(sum(arr))
+#Ответ: 234168
 
 
 # Задача 5
@@ -72,5 +72,5 @@ if ca > do:
     print('cat')
 else:
     print('dog')
-#Ответ:
+#Ответ: cat
 
