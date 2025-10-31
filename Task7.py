2f0de5cc37eca5205fe9119e60ba0f3aaaeab499
LokEnce333 <ilya.evstratoff@yandex.ru>
2025-10-31 23:29:31 +0300

    Task #7

---
+++ Task7.py
@@ -0,0 +1,25 @@
+# # Задача 1
+# print(sum([x*x for x in range(1, 101)]))
+# # Ответ: 338350
+
+
+# # Задача 2
+
+# # Ответ: [0, 8, 16, 24, 32]
+
+
+# # Задача 3
+# print(sum([1 for x in range(1, 21) if x % 2 == 0]))
+# # Ответ: 10
+
+
+# # Задача 4
+# arr = list([int(input()) for _ in range(10)])
+# print(sum([1 for i in range(0, len(arr), 2) if arr[i] % 2 == 0]))
+# # Ответ: 3
+
+
+# Задача 5
+print(len([x for x in range(1, 1001) if (x % 7 == 0) or (x % 11 == 0)]))
+# Ответ:
+
